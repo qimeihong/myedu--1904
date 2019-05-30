e5921bfe00af88931dc4f5095ac569acc73775b0
祁美宏 <3268602292@qq.com>
2019-05-30 11:10:56 +0800

    first commin

---
+++ day01/base_type.py
@@ -0,0 +1,210 @@
+
+
+
+
+
+
+
+def int_demo():
+    aint=5
+    print(type(aint))
+def aa():
+    d=3
+    c=3
+    b=6.52
+    w='12ghj'
+    print('%s,%s,%s,%s'%(d,c,b,w))
+
+def ggg():
+    yui=3.0
+    gei=10
+    wer='19'
+    print(type(yui))
+    print(type(gei))
+    print(type(wer))
+
+
+
+
+
+
+
+
+
+
+
+
+
+def qww():
+    qa=123.0
+    sd=45
+    rt='789'
+    print('%s,%s,%s'%(qa,sd,rt))
+
+
+
+
+
+
+
+
+
+
+qm=123
+print(type(qm))
+
+
+yu=123
+
+
+def add(num1,num2):
+    print(num1)
+    print(num2)
+    print(num1+num2)
+    return num1+num2
+def q1(a1,b2):
+    print(a1)
+    print(b2)
+    print(a1+b2)
+    return a1+b2
+
+
+def str_join():
+    a=7
+    b='6'
+    c='3'
+    print(b+c)
+    print(str(a)+b)
+    print('a是%s,b是%s'%(a,b))
+    print('a是%s,b是%s,c是%s'%(a,b,c))
+
+def type2():
+    b=100
+    print(type(b))
+    b=str(b)
+    print(type(b))
+
+def type1():
+    b='100'
+    print(type(b))
+    b=int(b)
+    print(b)
+
+q=7
+
+def str_demo():
+    astr='5'
+    print(type(astr))
+
+def int_demo():
+    aint=5
+    print(type(aint))
+
+def aaa():
+    aaa=1.1
+    print(type(aaa))
+
+
+def ttt():
+    w=123
+    e='456'
+    print(type(w))
+    w=str(w)
+    print(w+e)
+    print('%s%s'%(w,e))
+
+#
+def yyy():
+    d=123
+    s='GHHJ'
+    c=12.5
+    print('%s,%s,%s'%(d,s,c))
+
+
+def how():
+    g=5852
+    j='0213'
+    print(type(g))
+    g=str(g)
+    print(j+g)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def  uui():
+    f=456
+    k='582'
+    m=58.32
+    o='789'
+    print('%s,%s,%s,%s'%(f,k,m,o))
+    print(type(f))
+    print(type(k))
+    print(type(m))
+    print(type(o))
+
+def yui(tyu,jui):
+    print(type(tyu))
+    print(type(jui))
+    return tyu+jui
+
+def aui(t1,t2):
+    print(type(t1))
+    print(type(t2))
+    return t1+t2
+
+
+
+
+
+
+
+#这是一个main方法，可以直接执行，main方法下面不能有其它方法
+if __name__ == '__main__':
+
+    # a =str_join()
+    # b = add(5,30)
+    # print(a)
+    # print(type(a))
+    # print(b)
+    #
+    # b =q1(5,30)
+    # print(a)
+    # print(type(a))
+    # print(b)
+    # type1()
+    # ttt()
+    # yyy()
+    # print(type(qm))
+    # how()
+    # uui()
+    # print(type(yu))
+    # d=yui(12,58)
+    # print(d)
+    # q=aui(15,15)
+    # print(q)
+    # int_demo
+    # int_demo
+    # aa()
+    # qww()
+    ggg()
+
+    h = 'asdfghjpoiopi'
+    print(h[4:])
+    print(h[0])
+    print(h[1:3])
+    print(h[-1])
+    fg = 'poiuytre'
+    print(fg[5:])
+    print(fg[0])
+    print(fg[])
---
+++ day01/作业练习.py
@@ -0,0 +1,22 @@
+#调换模块公式
+from  day01 import base_type
+
+def te_st1():
+
+    print('test1')
+
+def te_st2():
+
+    print('test2')
+
+
+
+def te_st3():
+    print('test3')
+
+
+if __name__ =='__main__':
+#模块内容
+    value =base_type.add(30,30)
+
+
