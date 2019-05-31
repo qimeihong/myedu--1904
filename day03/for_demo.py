2669c776000d8da3c00fd097e939c9e891ca6a80
祁美宏 <3268602292@qq.com>
2019-05-31 10:59:49 +0800

    day033

---
+++ day03/for_demo.py
@@ -0,0 +1,12 @@
+def for_demo():
+    for i in range(7):
+        print('hell world')
+        print(i)
+
+def for_demo1():
+    for i in range(4,9):
+        print('hello world')
+        print(i)
+
+if __name__ == '__main__':
+    for_demo1()
