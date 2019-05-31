2a177e1daa28c804671b0e9df46c2d4b8c63aee8
祁美宏 <3268602292@qq.com>
2019-05-31 10:17:46 +0800

    day03

--- day03/if_else.py
+++ day03/if_else.py
@@ -2,8 +2,24 @@ def if_demo():
     a=3>4
     if a :
         print('a 是 True')
+
     else:
         print('a 是 Flase')
 
+def elif_demo():
+    a =2
+    if a==1:
+        print('a是1')
+    elif a==2:
+        print('a是2')
+    elif a==3:
+        print('a是3')
+    elif a==4:
+        print('a是4')
+
+
+
+
 if __name__ == '__main__':
-    if_demo()
+    # if_demo()
+    elif_demo()
